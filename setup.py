76e01de691793b227ffd9782cfb63250ec2467c0
the-gigi <the.gigi@gmail.com>
2020-08-23 15:14:49 -0700

    bump to 0.3.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='dominion-grpc-client',
-      version='0.2.0',
+      version='0.3.0',
       url='https://github.com/the-gigi/dominion-grpc-client-py',
       license='MIT',
       author='Gigi Sayfan',
